05f5dc01aedb56512c2737f634debf0640bd9a7f
rondo <58355355+RondoChow@users.noreply.github.com>
2021-01-05 16:26:36 +0800

    First

---
+++ cmake-build-debug/CMakeFiles/Library_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Library_management_system.dir/main.cpp.o"
+  "Library_management_system"
+  "Library_management_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Library_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
